b628062cc47bc3c938f4974f0d023e0fb8f6e022
Christine Demetillo <mgd22@njit.edu>
2019-12-18 13:14:50 -0500

    Feature: Corrected due to tutorial

--- blog-api/src/config.py
+++ blog-api/src/config.py
@@ -1,7 +1,9 @@
 # /src/config.py
 
 import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 class Development(object):
     """
@@ -9,6 +11,7 @@ class Development(object):
     """
     DEBUG = True
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
@@ -18,14 +21,9 @@ class Production(object):
     """
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-
-
-app_config = {
-    'development': Development,
-    'production': Production,
-}
 
 class Testing(object):
     """
@@ -36,9 +34,8 @@ class Testing(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
- app_config = {
-  #####################
-    # existing code here#
-    #####################
+app_config = {
+    'development': Development,
+    'production': Production,
     'testing': Testing
- }
+}
